13d0c4342c0d8b99aad059dd395828bcfbe7dcea
bruno-calasans <brunogstsho@gmail.com>
2025-11-16 22:30:07 -0300

    create android build

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text className="font-bold text-red-500">Merda</Text>
+      <Text className="font-bold text-green-500">Merda</Text>
       <Text className="font-bold text-indigo-500">Merda</Text>
     </View>
   );
